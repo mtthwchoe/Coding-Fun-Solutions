2d361975eece75afa5b4ae8394f27de60583cf31
mtthwchoe <matthewjinkyuchoe@gmail.com>
2022-12-25 00:40:53 -0800

    fountain done... needs work

--- Kattis/Fountain/test.cpp
+++ Kattis/Fountain/test.cpp
@@ -3,10 +3,17 @@
 #include <vector>
 using namespace std;
 
+void print_map(std::vector<std::string> all) {
+	for(int i = 0; i < all.size(); i++) {
+		cout << all[i] + "\n";
+	}
+}
+
 int main() {
     
     int rows;
 	int columns;
+	int flag = 0;
 	string temp;
 	
 	cin >> rows;
@@ -18,12 +25,30 @@ int main() {
 		cin >> temp;
 		all.push_back(temp);
 	}
-	
-	for(string i : all) {
-		cout << i;
-		cout << "\n";
+	for(int i = 0; i < 3; i++) {
+		for(int r = 0; r < rows; r++) {
+			for(int c = 0; c < columns; c++) {
+				if(all[r][c] == 'V') {
+					if(r + 1 < rows && all[r+1][c] == '.') {
+						all[r+1][c] = 'V';
+					}
+					else if(r + 1 < rows && all[r+1][c] == '#') {
+						if(c - 1 >= 0 && all[r][c-1] == '.') {
+							all[r][c-1] = 'V';
+							flag++;
+						}
+						if(c + 1 < columns && all[r][c+1] == '.') {
+							all[r][c+1] = 'V';
+						}
+						if(flag == 1) {
+							c = 0;
+						}
+						flag = 0;
+					}
+				}
+			}
+		}
 	}
-	
-
+	print_map(all);
     return(0);
 }
---
+++ Leetcode/1/my solution.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut x:i32 = 0;
+        let mut y:i32 = 0;
+        for i in 0..nums.len(){
+            for j in 0..nums.len() {
+                if(nums[i] + nums[j] == target && i != j) {
+                    x = i as i32;
+                    y = j as i32;
+                }
+            }
+        }
+        let r:Vec<i32> = vec![x,y];
+        r
+    }
+}
