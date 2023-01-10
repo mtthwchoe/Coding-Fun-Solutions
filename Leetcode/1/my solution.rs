16e346b2b4d3d83b80fe21415685319c50711966
mtthwchoe <matthewjinkyuchoe@gmail.com>
2023-01-10 04:52:07 -0800

    cleaned up

--- Leetcode/1/my solution.rs
+++ Leetcode/1/my solution.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut x:i32 = 0;
         let mut y:i32 = 0;
-        for i in 0..nums.len(){
+        for i in 0..nums.len() {
             for j in 0..nums.len() {
                 if(nums[i] + nums[j] == target && i != j) {
                     x = i as i32;
